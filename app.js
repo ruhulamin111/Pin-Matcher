9fd3c2251f73e456c130e00d2f5f36ac8f956315
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-06 08:41:20 +0600

    pin match problem solve

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ function pinGenerate() {
     const pinShow = document.getElementById('pin-show').value = getPin;
 }
 
+function verifyPin() {
+    const pin = document.getElementById('pin-show').value;
+    const pinType = document.getElementById('pin-type').value;
+    const fail = document.getElementById('fail')
+    const success = document.getElementById('success')
+    if (pin == pinType) {
+        success.style.display = 'block';
+        fail.style.display = 'none';
+    }
+    else {
+        const fail = document.getElementById('fail')
+        fail.style.display = 'block';
+        success.style.display = 'none';
+    }
+}
+
 document.getElementById('keypad').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const numberInput = document.getElementById('pin-type');
